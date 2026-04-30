1fa02ab263094915e0ca7f72f581a02f7ca3c1bf
CRIMINAL <butani.hardik53@gmail.com>
2026-04-30 22:48:23 +0530

    feat: resolve the error coming on all the page load

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,14 @@ const clientRoutes = require('./clientRoutes');
 const projectRoutes = require('./projectRoutes');
 const taskRoutes = require('./taskRoutes');
 const userRoutes = require('./userRoutes');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // Mount routes
 router.use('/auth', authRoutes);
+
+// Protect all following routes
+router.use(authMiddleware);
+
 router.use('/clients', clientRoutes);
 router.use('/projects', projectRoutes);
 router.use('/tasks', taskRoutes);
